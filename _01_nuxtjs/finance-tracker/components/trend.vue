1da2c347a5e377117cecdb60a38f6ea8e14576cb
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-29 22:51:02 -0300

    Hydration, Reactivity & Data

--- _01_nuxtjs/finance-tracker/components/trend.vue
+++ _01_nuxtjs/finance-tracker/components/trend.vue
@@ -1,22 +1,22 @@
 <template>
-    <div>
-        <div class="font-bold" :class="[color]">{{ title }}</div>
+  <div>
+    <div class="font-bold" :class="[color]">{{ title }}</div>
 
-        <div class="text-2xl font-extrabold text-black dark:text-white mb-2">
-            <USkeleton class="h-8 w-full" v-if="loading" />
-            <div v-else>{{ currency }}</div>
-        </div>
-
-        <div>
-            <USkeleton class="h-6 w-full" v-if="loading" />
-            <div v-else class="flex space-x-1 items-center text-sm">
-                <UIcon :name="icon" class="w-6 h-6" :class="{ 'green': trendingUp, 'red': !trendingUp }" />
-                <div class="text-gray-500 dark:text-gray-400">
-                    {{ percentageTrend }} vs last period
-                </div>
-            </div>
-        </div>
+    <div class="text-2xl font-extrabold text-black dark:text-white mb-2">
+      <USkeleton class="h-8 w-full" v-if="loading" />
+      <div v-else>{{ currency }}</div>
     </div>
+
+    <div>
+      <USkeleton class="h-6 w-full" v-if="loading" />
+      <div v-else class="flex space-x-1 items-center text-sm">
+        <UIcon :name="icon" class="w-6 h-6" :class="{ 'green': trendingUp, 'red': !trendingUp }" />
+        <div class="text-gray-500 dark:text-gray-400">
+          {{ percentageTrend }} vs last period
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -27,15 +27,14 @@ const props = defineProps({
   color: String,
   loading: Boolean
 })
-
+const { amount } = toRefs(props)
 const trendingUp = computed(
   () => props.amount >= props.lastAmount
 )
 const icon = computed(
   () => trendingUp.value ? 'i-heroicons-arrow-trending-up' : 'i-heroicons-arrow-trending-down'
 )
-
-const { currency } = useCurrency(props.amount)
+const { currency } = useCurrency(amount)
 
 const percentageTrend = computed(() => {
   if (props.amount === 0 || props.lastAmount === 0) return '∞%'
--- _01_nuxtjs/finance-tracker/pages/index.vue
+++ _01_nuxtjs/finance-tracker/pages/index.vue
@@ -1,109 +1,69 @@
 <template>
     <div>
-
         <section class="flex items-center justify-between mb-10">
-            <h1 class="text-4xl font-extrabold">Summary</h1>
-            
-            <div>
-                <USelectMenu  :options="transactionViewOptions" v-model="selectedView" />
-            </div>
-            
+        <h1 class="text-4xl font-extrabold">
+            Summary
+        </h1>
+        <div>
+            <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
+        </div>
         </section>
-        
+    
         <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-            <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="4100" :loading="isLoading" />
-            <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="4100" :loading="isLoading" />
-            <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="isLoading" />
-            <Trend color="red" title="Saving" :amount="4000" :last-amount="4100" :loading="isLoading" />
+        <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="prevIncomeTotal" :loading="pending" />
+        <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="prevExpenseTotal" :loading="pending" />
+        <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="pending" />
+        <Trend color="red" title="Saving" :amount="4000" :last-amount="4100" :loading="pending" />
         </section>
-
+    
         <section class="flex justify-between mb-10">
-            <div>
-                <h2 class="text-2xl font-extrabold">Transactions</h2>
-            
-                <div class="text-gray-500 dark:text-gray-400">
-                    You have {{ incomeCount }} incomes and {{ expenseCount }} expenses this period
-                </div>
+        <div>
+            <h2 class="text-2xl font-extrabold">Transactions</h2>
+            <div class="text-gray-500 dark:text-gray-400">
+            You have {{ incomeCount }} incomes and {{ expenseCount }} expenses this period
             </div>
-            
-            <div>
-                <TransactionModal v-model="isOpen" @saved="refresh()" />
-                <UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" @click="isOpen = true" />
-            </div>
+        </div>
+        <div>
+            <TransactionModal v-model="isOpen" @saved="refresh()" />
+            <UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" @click="isOpen = true" />
+        </div>
         </section>
-
+    
         <section v-if="!pending">
-            <div v-for="(transactionsOnDay, date) in byDate" :key="date" class="mb-10">
-                <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
-                <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction"
-                    @deleted="refresh()" @edited="refresh()" />
-            </div>
+        <div v-for="(transactionsOnDay, date) in byDate" :key="date" class="mb-10">
+            <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
+            <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction"
+            @deleted="refresh()" @edited="refresh()" />
+        </div>
         </section>
         <section v-else>
-            <USkeleton class="h-8 w-full mb-2" v-for="i in 4" :key="i" />
+        <USkeleton class="h-8 w-full mb-2" v-for="i in 4" :key="i" />
         </section>
     </div>
 </template>
-
-<script setup>
-import {transactionViewOptions} from '~/constants'
-
-const selectedView = ref(transactionViewOptions[1])
-
-const supabase = useSupabaseClient()
-
-const transactions = ref([])
-
-const isLoading = ref(false)
-
-const income = computed(() => transactions.value.filter(transaction => transaction.type === 'Income'))
-const expense = computed(() => transactions.value.filter(transaction => transaction.type === 'Expense'))
-
-const incomeCount = computed(() => income.value.length)
-const expenseCount = computed(() => expense.value.length)
-
-const incomeTotal = computed(() => income.value.reduce((total, transaction) => total + transaction.amount, 0))
-const expenseTotal = computed(() => expense.value.reduce((total, transaction) => total + transaction.amount, 0))
-
-const fetchTransactions = async() => {
-    isLoading.value = true
-
-    try {
-        const { data } = await useAsyncData('transactions', async() => {
-            const { data, error } = await supabase
-                .from('transactions')
-                .select()
-        
-            if (error) throw error
-
-            return data
-        })
-
-        return data.value
-    } finally {
-        isLoading.value = false
+  
+  <script setup>
+  import { transactionViewOptions } from '~/constants'
+  
+  const user = useSupabaseUser()
+  const selectedView = ref(user.value.user_metadata?.transaction_view ?? transactionViewOptions[1])
+  const isOpen = ref(false)
+  const { current, previous } = useSelectedTimePeriod(selectedView)
+  
+  const { pending, refresh, transactions: {
+    incomeCount,
+    expenseCount,
+    incomeTotal,
+    expenseTotal,
+    grouped: {
+      byDate
     }
-}
-
-const refresh = async() => {
-    await fetchTransactions()
-}
-
-transactions.value = refresh()
-
-const transactionsGroupedByDate = computed(() => {
-    let  grouped = {}
-    
-    for (const transaction of transactions.value) {
-        const date = new Date(transaction.created_at).toISOString().split('T')[0]
-
-        if(!grouped[date]) {
-            grouped[date] = []
-        }
-
-        grouped[date].push(transaction)
-    }
-
-    return grouped
-})
-</script>
+  } } = useFetchTransactions(current)
+  
+  const { refresh: refreshPrevious, transactions: {
+    incomeTotal: prevIncomeTotal,
+    expenseTotal: prevExpenseTotal,
+  } } = useFetchTransactions(previous)
+  
+  await Promise.all([refresh(), refreshPrevious()])
+  </script>
