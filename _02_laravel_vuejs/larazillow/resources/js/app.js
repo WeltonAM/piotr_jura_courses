e6ad859acd0568c8083eb82ac649b22076486cb9
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-10-30 22:53:51 -0300

    Styling

--- _02_laravel_vuejs/larazillow/resources/js/app.js
+++ _02_laravel_vuejs/larazillow/resources/js/app.js
@@ -2,16 +2,19 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import '../css/app.css'
 import MainLayout from './Layouts/MainLayout.vue'
-import { ZiggyVue } from 'ziggy-js';
-
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+
     let page = pages[`./Pages/${name}.vue`]
+
     page.default.layout = page.default.layout || MainLayout
+
     return page
   },
+
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
--- _02_laravel_vuejs/larazillow/resources/views/app.blade.php
+++ _02_laravel_vuejs/larazillow/resources/views/app.blade.php
@@ -4,13 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Larazillow</title>
+        <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
 
     </head>
-    <body>
+    <body class="bg-white text-gray-800 dark:bg-gray-900 dark:text-gray-300">
         @inertia
     </body>
 </html>
